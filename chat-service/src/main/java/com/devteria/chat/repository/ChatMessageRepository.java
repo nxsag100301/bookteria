c1dfd473a4263c9b5c5455fea650caf4bd253f8e
nxsag100301 <nguyenxuansang10032001@gmail.com>
2026-08-15 13:47:42 +0700

    first fork

--- chat-service/src/main/java/com/devteria/chat/repository/ChatMessageRepository.java
+++ chat-service/src/main/java/com/devteria/chat/repository/ChatMessageRepository.java
@@ -1,10 +1,11 @@
 package com.devteria.chat.repository;
 
-import com.devteria.chat.entity.ChatMessage;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.devteria.chat.entity.ChatMessage;
 
 @Repository
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
--- chat-service/src/main/java/com/devteria/chat/service/ConversationService.java
+++ chat-service/src/main/java/com/devteria/chat/service/ConversationService.java
@@ -1,5 +1,14 @@
 package com.devteria.chat.service;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
 import com.devteria.chat.dto.request.ConversationRequest;
 import com.devteria.chat.dto.response.ConversationResponse;
 import com.devteria.chat.entity.Conversation;
@@ -9,18 +18,11 @@ import com.devteria.chat.exception.ErrorCode;
 import com.devteria.chat.mapper.ConversationMapper;
 import com.devteria.chat.repository.ConversationRepository;
 import com.devteria.chat.repository.httpclient.ProfileClient;
+
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Service;
-
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.StringJoiner;
 
 @Slf4j
 @Service
@@ -43,8 +45,8 @@ public class ConversationService {
         // Fetch user infos
         String userId = SecurityContextHolder.getContext().getAuthentication().getName();
         var userInfoResponse = profileClient.getProfile(userId);
-        var participantInfoResponse = profileClient.getProfile(
-                request.getParticipantIds().getFirst());
+        var participantInfoResponse =
+                profileClient.getProfile(request.getParticipantIds().getFirst());
 
         if (Objects.isNull(userInfoResponse) || Objects.isNull(participantInfoResponse)) {
             throw new AppException(ErrorCode.UNCATEGORIZED_EXCEPTION);
@@ -60,7 +62,8 @@ public class ConversationService {
         var sortedIds = userIds.stream().sorted().toList();
         String userIdHash = generateParticipantHash(sortedIds);
 
-        var conversation = conversationRepository.findByParticipantsHash(userIdHash)
+        var conversation = conversationRepository
+                .findByParticipantsHash(userIdHash)
                 .orElseGet(() -> {
                     List<ParticipantInfo> participantInfos = List.of(
                             ParticipantInfo.builder()
@@ -76,8 +79,7 @@ public class ConversationService {
                                     .firstName(participantInfo.getFirstName())
                                     .lastName(participantInfo.getLastName())
                                     .avatar(participantInfo.getAvatar())
-                                    .build()
-                    );
+                                    .build());
 
                     // Build conversation info
                     Conversation newConversation = Conversation.builder()
@@ -104,13 +106,15 @@ public class ConversationService {
     }
 
     private ConversationResponse toConversationResponse(Conversation conversation) {
-        String currentUserId = SecurityContextHolder.getContext().getAuthentication().getName();
+        String currentUserId =
+                SecurityContextHolder.getContext().getAuthentication().getName();
 
         ConversationResponse conversationResponse = conversationMapper.toConversationResponse(conversation);
 
         conversation.getParticipants().stream()
                 .filter(participantInfo -> !participantInfo.getUserId().equals(currentUserId))
-                .findFirst().ifPresent(participantInfo -> {
+                .findFirst()
+                .ifPresent(participantInfo -> {
                     conversationResponse.setConversationName(participantInfo.getUsername());
                     conversationResponse.setConversationAvatar(participantInfo.getAvatar());
                 });
