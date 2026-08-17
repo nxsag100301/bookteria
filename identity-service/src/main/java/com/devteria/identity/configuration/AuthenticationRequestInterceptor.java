a467cd88ce4632dc37873f944c226aae9baf0904
nxsag100301 <nguyenxuansang10032001@gmail.com>
2026-08-17 09:46:37 +0700

    no7

---
+++ identity-service/src/main/java/com/devteria/identity/configuration/AuthenticationRequestInterceptor.java
@@ -0,0 +1,22 @@
+package com.devteria.identity.configuration;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+public class AuthenticationRequestInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate template) {
+        ServletRequestAttributes servletRequestAttributes =
+                (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        String authHeader = servletRequestAttributes.getRequest().getHeader("Authorization");
+        if (StringUtils.hasText(authHeader)) {
+            template.header("Authorization", authHeader);
+        }
+    }
+}
