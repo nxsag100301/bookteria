73a7f69b1428b30916a3ae036c9df99ba4246ce8
nxsag100301 <nguyenxuansang10032001@gmail.com>
2026-08-17 13:32:56 +0700

    no9

--- api-gateway/src/main/java/com/devteria/gateway/configuration/WebClientConfiguration.java
+++ api-gateway/src/main/java/com/devteria/gateway/configuration/WebClientConfiguration.java
@@ -1,8 +1,15 @@
 package com.devteria.gateway.configuration;
 
 import com.devteria.gateway.repository.IdentityClient;
+
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -10,14 +17,27 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Configuration
 public class WebClientConfiguration {
     @Bean
-    WebClient webClient(){
+    WebClient webClient() {
         return WebClient.builder()
                 .baseUrl("http://localhost:8080/identity")
                 .build();
     }
 
     @Bean
-    IdentityClient identityClient(WebClient webClient){
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
+
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
+    }
+
+    @Bean
+    IdentityClient identityClient(WebClient webClient) {
         HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory
                 .builderFor(WebClientAdapter.create(webClient)).build();
 
