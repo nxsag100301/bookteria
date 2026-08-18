c390d7cf8cbfc974fea2c6030e0be953fd398ae4
nxsag100301 <nguyenxuansang10032001@gmail.com>
2026-08-18 10:45:11 +0700

    no13

--- profile-service/src/main/java/com/devteria/notification/controller/UserProfileController.java
+++ profile-service/src/main/java/com/devteria/notification/controller/UserProfileController.java
@@ -1,5 +1,11 @@
 package com.devteria.notification.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.devteria.notification.dto.ApiResponse;
 import com.devteria.notification.dto.response.UserProfileResponse;
 import com.devteria.notification.service.UserProfileService;
@@ -7,11 +13,6 @@ import com.devteria.notification.service.UserProfileService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
